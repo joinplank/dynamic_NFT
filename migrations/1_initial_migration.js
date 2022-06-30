a7b397e511667fb3226ba2e58dc0442a44acd648
Julian <julian.schtutman@joinplank.com>
2022-06-30 11:12:40 -0300

    Forder Organization

--- migrations/1_initial_migration.js
+++
@@ -1,5 +0,0 @@
-const contract = artifacts.require("carRegistrationContract");
-
-module.exports = function (deployer) {
-  deployer.deploy(contract, 2); //2 is an argument the contructor expects for the fee
-};
